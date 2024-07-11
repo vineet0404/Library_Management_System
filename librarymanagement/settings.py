23d91a9f87fba2548dffc1687b8fe31035e66547
Vineet Singh <83958545+vineet0404@users.noreply.github.com>
2024-07-11 22:46:37 +0530

    Update settings.py

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k(6-_4sgig$sbrccr2*0=7s)bklpiywi#_121l)s(f8)dq^^wv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 STATIC_DIR,
